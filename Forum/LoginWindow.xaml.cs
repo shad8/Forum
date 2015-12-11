16d4f6d1a5e4cb2a3bf451d770fccd3cb48e83b3
shad8 <uholodniak@gmail.com>
2015-12-11 20:58:39 +0100

    Create window login form

--- Forum/LoginWindow.xaml.cs
+++ Forum/LoginWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Data;
-using System.Data.Entity;
 
 namespace Forum
 {
   /// <summary>
-  /// Interaction logic for MainWindow.xaml
+  /// Interaction logic for LoginWindow.xaml
   /// </summary>
-  public partial class MainWindow : Window
+  public partial class LoginWindow : Window
   {
-    public MainWindow()
+    public LoginWindow()
     {
       InitializeComponent();
-      ForumContext context = new ForumContext();
-      LoginWindow LoginWindow = new LoginWindow();
-      LoginWindow.Show();
-      this.Hide();
     }
   }
 }
